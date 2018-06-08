d50a8fe3010b65a53ce76e0a785a301511c2404a
saiminhtet <htetsaimin@gmail.com>
2018-06-08 09:53:50 +0800

    disabled security autoconfiguration class

--- LifeStyle_Club_Facility_Booking/src/main/java/com/lsc/mvc/LifeStyleClubFacilityBookingApplication.java
+++ LifeStyle_Club_Facility_Booking/src/main/java/com/lsc/mvc/LifeStyleClubFacilityBookingApplication.java
@@ -2,8 +2,9 @@ package com.lsc.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class LifeStyleClubFacilityBookingApplication {
 
 	public static void main(String[] args) {
